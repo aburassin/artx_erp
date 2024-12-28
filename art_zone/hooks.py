769f563be94e9305c5045b31e74a34260415636b
abdelwahabdev <abdelwahab.00964@gmail.com>
2024-12-28 12:54:44 +0200

    add art zone workspace

--- art_zone/hooks.py
+++ art_zone/hooks.py
@@ -83,7 +83,7 @@ app_license = "mit"
 # ------------
 
 # before_install = "art_zone.install.before_install"
-after_install = ["art_zone.after_install.after_install"]
+# after_install = ["art_zone.after_install.after_install"]
 
 # Uninstallation
 # ------------
